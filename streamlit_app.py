0900f4893ce5855f9ad9ba298a17e68949f4eff2
Ivan Carlos Zheraldovich <izheraldovich@comcast.net>
2024-06-28 14:45:14 -0700

    Update streamlit_app.py
    add dataframe to fruityvice_response

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,4 @@ if time_to_insert:
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_didth=True)
